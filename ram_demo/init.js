ac3749592fda3d48cc4b74a34c1163b12fb39d75
Barni <mezeibg@gmail.com>
2025-03-27 19:05:46 +0100

    Reworked the layout of the RAM demo

--- ram_demo/init.js
+++ ram_demo/init.js
@@ -1,3 +1,5 @@
 let RAM = [];
 let WORD_SIZE = 16;
 let RAM_SIZE = Math.pow(2, WORD_SIZE);
+
+let address_division_step = 128;
--- ram_demo/main.js
+++ ram_demo/main.js
@@ -1,12 +1,7 @@
-document.documentElement.style.cssText = "--main-background-color: red";
+let addressContainer = document.getElementById("address_line");
 
-let address_line_container = document.getElementById("address_line");
-
-for (let address = 0; address < RAM_SIZE; address++) {
-    if (address % 100 == 0) {
-        let new_address = document.createElement("p");
-        new_address.classList = "address";
-        new_address.textContent = address;
-        address_line_container.appendChild(new_address);
-    }
-}
+/*for (let i = 0; i < 128; i++) {
+    let newAddress = document.createElement("p");
+    newAddress.textContent = i;
+    addressContainer.appendChild(newAddress);
+}*/
